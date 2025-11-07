7ee4cb3bb0fb58cad5be3b8ffc9406cbf0bbff07
Ian Macartney <ianhmacartney@gmail.com>
2025-11-07 12:33:05 -0800

    fix some type errors

--- example/app/App.tsx
+++ example/app/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Demo } from "./Demo";
 import { registerRootComponent } from "expo";
-import defaultConfig from "@tamagui/config/v3";
+import { config as defaultConfig } from "@tamagui/config/v3";
 import { createTamagui, TamaguiProvider } from "tamagui";
 
 const config = createTamagui(defaultConfig);
