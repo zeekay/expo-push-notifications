ad9dacde686eb348dc3538c654718df2509642ee
Ian Macartney <ianhmacartney@gmail.com>
2026-01-06 12:30:41 -0800

    explicit color schema

--- example/app/(tabs)/_layout.tsx
+++ example/app/(tabs)/_layout.tsx
@@ -6,7 +6,7 @@ import { Colors } from "@/constants/theme";
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme() === "dark" ? "dark" : "light";
 
   return (
     <Tabs
